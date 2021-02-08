07d05e422edde69873b33e4fab2873a3c98cb0c0
h6k2 <sepak.hirokazu10@gmail.com>
2021-02-08 23:19:14 +0900

    Header component追加

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header-wrap">
+    <div class="header-inner">
+      <h1 class="logo">
+        <img src="" alt="logo" />
+      </h1>
+      <h2>
+        Portfolio Sepak Life
+      </h2>
+    </div>
+    <!-- /.header-inner -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header-inner {
+  display: flex;
+  justify-content: space-between;
+}
+.logo {
+  width: 300px;
+  height: 50px;
+}
+h2 {
+  display: inline-block;
+  color: white;
+  line-height: 50px;
+  height: 50px;
+}
+</style>
