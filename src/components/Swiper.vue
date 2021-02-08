aae247e1d89aacd127341c5b33236e7319bd375d
h6k2 <sepak.hirokazu10@gmail.com>
2021-02-08 23:20:01 +0900

    Swiper component 追加

---
+++ src/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="banner in banners" :key="banner">
+      <img :src="banner" />
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper-button-next" slot="pagination"></div>
+    <div class="swiper-button-prev" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      banners: [
+        "./img/ogp_image.png",
+        "./img/KI204712.jpg",
+        "./img/ceremony.jpg",
+      ],
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+.swiper-wrapper {
+  margin-bottom: 30px;
+}
+
+.swiper-slide img {
+  height: 500px;
+  overflow: hidden;
+  object-fit: cover;
+}
+</style>
