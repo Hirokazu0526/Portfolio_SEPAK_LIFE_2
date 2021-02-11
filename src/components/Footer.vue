b24cda9c1fd0fff6e023da40d56e49c40b4a7267
h6k2 <sepak.hirokazu10@gmail.com>
2021-02-11 23:46:23 +0900

    footerコンポーネント追加

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p class="copy_right">&copy; 2021 Kobayashi Hirokazu</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.copy_right {
+  font-size: 1rem;
+  color: white;
+  margin: 50px auto 20px;
+}</style
+>>
