d13e34ba25085cebd5959f553a4bb3b5ad70f003
h6k2 <sepak.hirokazu10@gmail.com>
2021-02-11 23:47:02 +0900

    video.vue追加

---
+++ src/components/Video.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <video autoplay :src="video"></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Video",
+  data() {
+    return {
+      video: "video/sepak_life_pv.mp4",
+    };
+  },
+};
+</script>
+
+<style scoped>
+video {
+  width: 100%;
+  margin-bottom: 30px;
+}
+</style>
