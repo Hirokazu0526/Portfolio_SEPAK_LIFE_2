835cf8d3c551f3934fd21bc65f98710539e2cb58
h6k2 <sepak.hirokazu10@gmail.com>
2021-02-15 21:16:33 +0900

    テキスト編集

--- src/views/Performance.vue
+++ src/views/Performance.vue
@@ -5,17 +5,17 @@
     <div class="container">
       <ul class="works__thumbnails">
         <li v-for="work in works" :key="work.id">
-          <img :src="work.img" target="_blank" />
+          <img :src="work.img" alt="セパタクローの写真" />
           <div class="works__info">
             <h3>
-              <a href="#">{{ work.title }}</a>
+              <a :href="work.link" target="_blank">{{ work.title }}</a>
             </h3>
             <h4>制作担当</h4>
-            <p>デザイン / コーディング / リリース</p>
+            <p>{{ work.skillTheme }}</p>
             <p>{{ work.skill }}</p>
             <h4>コンセプト</h4>
             <p>
-              ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
+              {{ work.coment }}
             </p>
           </div>
         </li>
@@ -37,20 +37,42 @@ export default {
           id: 0,
           img: "images/works000.png",
           title: "第31回全日本セパタクロー選手権大会特設サイト ",
+          link: "https://sepaktakraw-alljapan2020.jp/",
+          skillTheme: "デザイン / コーディング / リリース",
           skill:
-            "Adobe XD Illustrator Photoshop / HTML5 CSS3 JavaScript VueCLI",
+            "Adobe XD Illustrator Photoshop / HTML5 CSS3 JavaScript Vue.js",
+          coment:
+            "「第31回全日本セパタクロー選手権大会」大会特設サイトのLP制作を担当させていただきました。グループステージの結果をテーブルにて表示することでスコアを分かりやすく表示した他、以前制作したサイトの仕様を踏襲しトンマナを保ちながらも違った雰囲気を演出いたしました。Vue.jsも取り入れコーディングやjs実装を効率的に行いました。",
         },
         {
           id: 1,
           img: "images/works001.jpg",
           title: "第27回全日本セパタクローオープン大会特設サイト ",
-          credit: "Adobe XD Illustrator Photoshop / HTML5 CSS3 JavaScript",
+          link: "https://sepaktakraw-open-championship.games/",
+          skillTheme: "デザイン / コーディング / リリース",
+          skill: "Adobe XD Illustrator Photoshop / HTML5 CSS3 JavaScript",
+          coment:
+            "「第27回全日本セパタクローオープン大会」大会特設サイトのLP制作を担当させていただきました。大会メインビジュアル、スケジュール、トーナメント表、大会結果、当日はライブ配信をサイト内に表示しました。アニメーションを加え、動きのあるサイトを制作しました。",
         },
         {
           id: 2,
+          img: "images/works002.png",
+          title: "YouTube オープニング制作事例_1 ",
+          link: "https://youtu.be/4PKNMz24ugA",
+          skillTheme: "映像編集",
+          skill: "Adobe AfterEffects Illustrator Photoshop",
+          coment:
+            "元のデザイン画ができておりましたので、Adobe AfterEffectsを利用しネオンの電光が文字を飾るエフェクトを入れ制作いたしました。",
+        },
+        {
+          id: 3,
           img: "images/works003.png",
-          title: "YouTube オープニング制作事例 ",
-          credit: "Photo By NAGAHAMA KOMEI",
+          title: "YouTube オープニング制作事例_2 ",
+          link: "https://youtu.be/EqgLmllZ-oI",
+          skillTheme: "映像編集",
+          skill: "Adobe AfterEffects PremierePro Illustrator Photoshop",
+          coment:
+            "短い時間の中に競技のダイナミックなプレーを入れ、インパクトを残すようにエフェクトを使い表現いたしました。",
         },
       ],
     };
@@ -80,7 +102,7 @@ export default {
 <style scoped>
 .performance {
   width: 1000px;
-  height: 1350px;
+  height: 1450px;
   margin: 0 auto;
 }
 .yellow {
