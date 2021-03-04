1127683c92e917faa544589c75a42b51814b60cc
h6k2 <sepak.hirokazu10@gmail.com>
2021-03-04 23:50:33 +0900

    テキスト修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <h2>
+      本サイトはVue CLIを使用したSPA(Single Page
+      Application)で制作しております。
+    </h2>
+    <div class="container">
+      <p>
+        Vue Routerのプラグインを利用しコンポーネントのルーティングを実装。<br />
+        Galleryコンポーネントではモーダルやカルーセルスライダーを実装し、<br />
+        選択した画像と紐付けて操作を行える様にしました。<br />
+        レスポンシブに対応している他、vue-analyticsのプラグインを利用しGoogleAnalyticsの設定をしております。
+      </p>
+      <br />
+      <h3>
+        <span class="ab">About</span>・<span class="pf">Performance</span
+        >・<span class="gl">Gallery</span> にて私のこれまでの<span class="st"
+          >STORY</span
+        >をご覧ください
+      </h3>
+    </div>
   </div>
 </template>
 
@@ -12,3 +30,55 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+.home {
+  width: 1000px;
+  margin: 0 auto;
+}
+
+h2,
+h3,
+p {
+  color: white;
+}
+p {
+  font-size: 20px;
+}
+
+h3 {
+  font-size: 25px;
+}
+
+.ab {
+  color: #ff04ff;
+}
+.pf {
+  color: #adff2e;
+}
+.gl {
+  color: #8b16f9;
+}
+.st {
+  color: cyan;
+}
+
+@media screen and (max-width: 599px) {
+  .home {
+    width: 100%;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  .container {
+    width: 90%;
+    margin: 0 auto;
+  }
+  p {
+    font-size: 11px;
+  }
+  h3 {
+    font-size: 15px;
+  }
+}
+</style>
