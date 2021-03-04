1a866bbd764e9b905d18c9b0523d75b7207f67f9
h6k2 <sepak.hirokazu10@gmail.com>
2021-03-04 22:12:27 +0900

    gaトラッキング追加

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'G-TXBCZZN8J0',
+  router
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -43,4 +43,6 @@ const router = new VueRouter({
   routes
 })
 
+
+
 export default router
