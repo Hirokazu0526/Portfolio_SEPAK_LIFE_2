8f942e4fef54c002d8868278c761007f5d2a0969
h6k2 <sepak.hirokazu10@gmail.com>
2021-03-18 09:17:23 +0900

    ログインページ追加

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    メールアドレス：<input class="login-box" type="text" v-model="email" />
+    <br />
+    パスワード：<input type="text" v-model="password" /> <br /><br />
+    <button @click="login">ログイン</button>
+  </div>
+</template>
+
+<script>
+import firebase from "../plugins/firebase";
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          () => {
+            alert("Success!");
+            this.$router.push("/imageupload");
+          },
+          (err) => {
+            alert(err.message);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  width: 1000px;
+  height: 200px;
+  margin: 0 auto;
+}
+.login-box {
+  margin: 20px 0;
+}
+</style>
