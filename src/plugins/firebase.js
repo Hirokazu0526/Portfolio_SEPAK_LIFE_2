108cceaaf6ea750905fc0adb9163922e55cf262b
h6k2 <sepak.hirokazu10@gmail.com>
2021-11-22 15:35:10 +0900

    firebase 修正

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
